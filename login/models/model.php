d3f5bb01e7e1455a1ea7eb374a8c5f9c0103700a
paulknisely <p.d.knisely@gmail.com>
2018-04-18 19:34:13 -0500

    staged files to ease the pain when we move the login module to document root. BE AWARE that login.php will need to be updated to update the location of the model files.

---
+++ login/models/model.php
@@ -0,0 +1,33 @@
+<?php
+//Check to see if the username and password are valid
+function checkValidUser() {
+    //validate user
+    $sql = "SELECT"; //sql statement here
+    //define values for parameters
+    $values = array(':username'=>$_POST['username'], ':pwd'=>md5($_POST['pwd']));
+    $result = getOneRecord($sql, $values);
+    return $result;
+}
+
+//Retrieve ONLY one record from the database
+function getOneRecord($sql, $parameter = null) {
+    global $db;
+    $statement = $db->prepare($sql);
+    //execute the SQL statement
+    $statement->execute($parameter);
+    //return the result
+    $result = $statement->fetch(PDO::FETCH::FETCH_ASSOC);
+    return $result;
+}
+
+//Retrieve multiple records from the database
+function getAllRecords($sql, $parameter = null) {
+    global $db;
+    $statement = $db->prepare($sql);
+    //execute the SQL statement
+    $statement->execute($parameter);
+    //return the result
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $result
+}
+?>
