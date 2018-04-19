88a9d2c1f06262a45266f00cfd1911f4e11f8ecc
paulknisely <p.d.knisely@gmail.com>
2018-04-19 00:43:15 -0500

    fix file path error at line 7

--- login/views/defaultview.php
+++ login/views/defaultview.php
@@ -4,7 +4,7 @@
 ?>
 <div class="container">
     <!-- REQUIRES ACTION -->
-    <form class="form-horizontal" action='../loginlogin.php?mode=checkLogin' method='post'>
+    <form class="form-horizontal" action='../login/login.php?mode=checkLogin' method='post'>
         <div class="form-group">
         <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
             <div class="col-sm-10">
