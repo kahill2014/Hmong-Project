df7eb9964a0b4a44f4dd5ee5cce591426933edb6
paulknisely <p.d.knisely@gmail.com>
2018-04-19 20:39:02 -0500

    add registration case

--- login/index.php
+++ login/index.php
@@ -9,6 +9,18 @@
     if (isset($_REQUEST['mode']))
         $mode = $_REQUEST['mode'];
     switch ($mode) {
+        case 'register':
+            $data = registerUser();
+            if (isset($data) && isset($data['email'])) {
+                $_SESSION['user'] = $data['lastName'].', '.$data['firstName'];
+                $_SESSION['email'] = $data['email'];
+                $_SESSION['username'] = $data['username'];
+            }
+            include('../login/pageFiles/pageheader.php');
+            include('../login/pageFiles/pagenav.php');
+            include('../login/views/defaultview.php');
+            include('../login/pageFiles/pagefooter.php');
+            break;
         case 'checkLogin':
             $data = checkValidUser();
             if (isset($data) && isset($data['email'])) {
